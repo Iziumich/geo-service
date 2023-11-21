3984080394dbf59a935977e41b8b29ea69606851
Andrey <143511905+Iziumich@users.noreply.github.com>
2023-11-21 16:02:35 +0300

    Update LocalizationServiceImplTest.java

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -1,7 +1,8 @@
+package ru.netology.i18n;
+
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
 import ru.netology.i18n.LocalizationServiceImpl;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LocalizationServiceImplTest {
 
